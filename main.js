ddfdc552dcbd0ea37ba6d706a67de72132f95772
mulwa06 <91142002+mulwa06@users.noreply.github.com>
2022-05-27 17:43:55 +0300

    imbalance, smoke and shadow remaining
    which includes their respective html

---
+++ main.js
@@ -0,0 +1,65 @@
+//print on web & console:
+    //document.write('<h2>I am a \'JAVASCRIPT\' Programmer!</h2>')
+    //console.log('herooo, this is the js console!')
+
+// display alert box:
+    //alert('This is an alert box! Welcome to my webpage!')
+
+
+
+
+
+// ml14
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml14 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml14 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeInOutExpo",
+    duration: 900
+  }).add({
+    targets: '.ml14 .letter',
+    opacity: [0,1],
+    translateX: [40,0],
+    translateZ: 0,
+    scaleX: [0.3, 1],
+    easing: "easeOutExpo",
+    duration: 800,
+    offset: '-=600',
+    delay: (el, i) => 150 + 25 * i
+  }).add({
+    targets: '.ml14',
+    opacity: 0,
+    duration: 3000,
+    easing: "easeOutExpo",
+    delay: 10000
+  });
+
+// ml3
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 10000
+  });
+
+
+
